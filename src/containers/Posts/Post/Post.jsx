6c6809fc905c58d33fa7b2a661e3b7289b57a15f
Borisov Semen <devsnice@gmail.com>
2018-01-27 20:29:58 +0300

    [+] - posts::added logic for rendering elements from reducer

---
+++ src/containers/Posts/Post/Post.jsx
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import styled from 'styled-components';
+
+import { Flex, Box } from 'grid-styled';
+
+import image from './image.jpg';
+
+const PostBox = styled(Flex).attrs({
+  justify: 'space-between'
+})`
+  margin-bottom: 20px;
+  width: 100%;
+  height: 225px;
+  border-radius: 6px;
+  border: 1px solid rgba(151, 151, 151, 0.29);
+  overflow: hidden;
+`;
+
+const PostImage = styled.img`
+  display: block;
+  height: 100%;
+  flex-basis: 275px;
+  flex-shrink: 0;
+  background-image: url("${props => props.image}");
+  background-position: center;
+  background-size: cover;
+`;
+
+const PostBody = styled(Box)`
+  padding: 20px 25px 20px 20px;
+`;
+
+const PostData = styled(Box)`
+  padding: 8px 0 18px 0;
+  font-size: 16px;
+  font-weight: 400;
+  color: #5f5f5f;
+`;
+
+const Title = styled.h2`
+  font-size: 18px;
+  line-height: 24px;
+  width: 450px;
+  max-width: 100%;
+  font-weight: 600;
+`;
+
+const Description = styled(Box)`
+  font-size: 15px;
+`;
+
+class Post extends Component {
+  static propTypes = {
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    city: PropTypes.string,
+    phone: PropTypes.string,
+    imgUrl: PropTypes.string
+  };
+
+  static defaultProps = {
+    phone: null,
+    city: null,
+    imgUrl: null
+  };
+
+  render() {
+    const { title, description, city, phone, imgUrl } = this.props;
+
+    return (
+      <PostBox>
+        <PostImage image={image} alt="post image" />
+        <PostBody>
+          <Title>{title}</Title>
+
+          <PostData>
+            {city} | {phone}
+          </PostData>
+
+          <Description>{description}</Description>
+        </PostBody>
+      </PostBox>
+    );
+  }
+}
+
+export default connect((state, props) => ({
+  ...state.posts.byId[props.id]
+}))(Post);
--- src/containers/Posts/Posts.jsx
+++ src/containers/Posts/Posts.jsx
@@ -1,13 +1,70 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import styled from 'styled-components';
 
-import postStoreService from '../../services/postsStoreService';
+import { Flex, Box } from 'grid-styled';
+
+import { postsRequest } from '../../ducks/posts';
+
+import Post from './Post/Post';
+
+const PostsLayout = styled(Flex).attrs({
+  justify: 'space-between'
+})`
+  margin-top: 45px;
+  width: 100%;
+`;
+
+const PostsBox = styled(Box)`
+  width: calc(100% - 315px);
+  margin-right: 35px;
+`;
+
+const FiltersBox = styled(Box)`
+  border: 1px solid rgba(151, 151, 151, 0.24);
+  border-radius: 6px;
+  flex-basis: 280px;
+  flex-shrink: 0;
+  height: 500px;
+  padding: 25px 18px;
+`;
 
 class Posts extends Component {
-  render() {
-    console.log(postStoreService.getAll());
+  static propTypes = {
+    postsIds: PropTypes.arrayOf(PropTypes.string).isRequired,
+    loaded: PropTypes.bool.isRequired,
+    loading: PropTypes.bool.isRequired,
+    error: PropTypes.bool.isRequired
+  };
 
-    return <div>Posts</div>;
+  componentDidMount() {
+    this.props.postsRequest();
+  }
+
+  renderPosts = () => {
+    const { postsIds } = this.props;
+
+    return postsIds.map(id => <Post key={id} id={id} />);
+  };
+
+  render() {
+    return (
+      <PostsLayout>
+        <PostsBox>{this.renderPosts()}</PostsBox>
+
+        <FiltersBox>place for filters</FiltersBox>
+      </PostsLayout>
+    );
   }
 }
 
-export default Posts;
+export default connect(
+  state => ({
+    postsIds: state.posts.postsIds,
+    loaded: state.posts.loaded,
+    loading: state.posts.loading,
+    error: state.posts.error
+  }),
+  { postsRequest }
+)(Posts);

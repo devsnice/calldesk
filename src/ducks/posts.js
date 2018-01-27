a74d37e208a8a5e0b19f10acdbc269bafd8422e1
Borisov Semen <devsnice@gmail.com>
2018-01-27 19:49:43 +0300

    [+] - posts::added logic of getting information from the store after first loading of page

--- src/ducks/posts.js
+++ src/ducks/posts.js
@@ -1,15 +1,97 @@
+import { createAction } from 'redux-actions';
+import { put, take, select, call } from 'redux-saga/effects';
+
+import postStoreService from '../services/postsStoreService';
+
 const initialState = {
-  posts: []
+  byId: {},
+  postsIds: [],
+  loaded: false,
+  loading: false,
+  error: false
 };
 
-const ACTIONS = {};
+const duckName = 'app/posts';
+
+const ACTIONS = {
+  POSTS_REQUEST: `${duckName}/POSTS_REQUEST`,
+  POSTS_REQUEST_SUCCESS: `${duckName}/POSTS_REQUEST_SUCCESS`,
+  POSTS_REQUEST_FAILURE: `${duckName}/POSTS_REQUEST_FAILURE`
+};
+
+/**
+ *  Reducer
+ */
 
 const reducer = (state = initialState, action) => {
   const { type, payload } = action;
 
   switch (type) {
+    case ACTIONS.POSTS_REQUEST: {
+      return {
+        ...state,
+        loading: true
+      };
+    }
+    case ACTIONS.POSTS_REQUEST_SUCCESS: {
+      return {
+        ...state,
+        postsIds: payload.postsIds,
+        byId: payload.posts,
+        loading: false,
+        loaded: true,
+        error: false
+      };
+    }
+    case ACTIONS.POSTS_REQUEST_FAILURE: {
+      return {
+        ...state,
+        loading: false,
+        loaded: true,
+        error: true
+      };
+    }
+
     default:
       return state;
+  }
+};
+
+/**
+ *  Action creater
+ */
+
+export const postsRequest = createAction(ACTIONS.POSTS_REQUEST);
+export const postsRequestSuccess = createAction(
+  ACTIONS.POSTS_REQUEST_SUCCESS,
+  ({ posts, postsIds }) => ({
+    posts,
+    postsIds
+  })
+);
+export const postsRequestFailure = createAction(ACTIONS.POSTS_REQUEST_FAILURE);
+
+/**
+ *  Saga
+ */
+
+export const loadPostsSaga = function*() {
+  while (true) {
+    const action = yield take(ACTIONS.POSTS_REQUEST);
+
+    try {
+      const posts = yield call(postStoreService.getAll);
+      const postsIds = Object.keys(posts);
+
+      yield put(
+        postsRequestSuccess({
+          posts,
+          postsIds
+        })
+      );
+    } catch (err) {
+      yield put(postsRequestFailure());
+    }
   }
 };
 
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,17 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
 import reducer from './reducer';
+import { loadPostsSaga } from '../ducks/posts';
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(sagaMiddleware)
 );
+
+sagaMiddleware.run(loadPostsSaga);
 
 export default store;

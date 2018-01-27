74c4522bdb7c0e07676a987178c62123746d4af6
Borisov Semen <devsnice@gmail.com>
2018-01-27 14:08:42 +0300

    [+] - store::added service for working with localstorage

--- src/containers/Posts/Posts.jsx
+++ src/containers/Posts/Posts.jsx
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 
+import postStoreService from '../../services/postsStoreService';
+
 class Posts extends Component {
   render() {
+    console.log(postStoreService.getAll());
+
     return <div>Posts</div>;
   }
 }
--- src/units/Layout/Layout.jsx
+++ src/units/Layout/Layout.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const Layout = styled.div`
-  width: 1280px;
+  max-width: 1280px;
   margin: 0 auto;
   padding: 60px 0;
 `;
